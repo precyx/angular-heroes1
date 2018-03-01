d53d6881d6f5f56f1ae4ccfbb1bd860503fff453
pquantum <pr.pascal.bachmann@gmail.com>
2018-03-01 09:08:07 +0100

    Revert "angular 5.2.6 Hero App"
    This reverts commit d32a6b7c5534dd9020de2bde556defde4d4f2e79.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
